e8a0170f809f4cecc1cf0761d2b23c6b71d1694d
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2024-07-07 16:11:47 +0530

    chore: Update investment calculator code to handle user input for investment amount, years, and expected return rate

--- investment-calculator.go
+++ investment-calculator.go
@@ -5,15 +5,21 @@ import (
 	"math" //Since in GO there isn't a standard library that allows us to do the power of a number, we need to import the math library
 )
 
+// Creating an investment calculator and learning about other Go basics
 func main() {
-	// fmt.Println("Hello, World!") // Print Hello, World!
-
-	//Creating an investment calculator and learning about other Go basics
 	const inflationRate float64 = 2.5 //The const keyword is used to declare a constant variable
-	var investmentAmount, years float64 = 1000, 10
-	var expectedReturnRate float64 = 5.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-	fmt.Scan(&investmentAmount) //The Scan function is used to read the input from the console thereby taking input from the user
+	fmt.Print("Enter the investment amount: ") //This print function does not add a new line at the end.
+	fmt.Scan(&investmentAmount)                //The Scan function is used to read the input from the console thereby taking input from the user
+
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years) //Within the math.Pow function, the first argument is the base and the second argument is the exponent
 	// var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years)) //This is another way to write the same code as above however, we are converting the investmentAmount and years to float64 when being used
